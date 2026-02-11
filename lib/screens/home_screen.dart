be62f39e6febb1b7ecf8995e4ebbcc655fad00b0
welkincedev <welkflutter@gmail.com>
2026-02-11 23:54:13 +0530

    Stage 2 complete: Firebase Auth login/signup

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AuthService authService = AuthService();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Take Notes"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await authService.logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => const LoginScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text("Logged In Successfully ✅", style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+}
